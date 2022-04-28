d4d57cbf60a087c3f395113534f4dbdc18381320
YusraAqeel12 <93079264+YusraAqeel12@users.noreply.github.com>
2022-04-28 22:32:39 +0500

    Add files via upload

---
+++ Data/DummyData.js
@@ -0,0 +1,168 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: 'Mens',
+    imageUrl:
+      'https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg',
+  },
+  {
+    id: 2,
+    name: 'Women',
+    imageUrl:
+      'https://talkstar-assets.s3.amazonaws.com/production/playlists/playlist_771/0be282cc-2e26-4517-b9ff-721c9608f645/fearless_women-2000x2000_2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Kids',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI3EyPVCvH_AHKyue8kwaE8QhQJqIeLhghBw&usqp=CAU',
+  },
+  {
+    id: 4,
+    name: 'Laptop',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Hgm9EuGPZJNpW2zdpW94vOKoOQz6oHnDNg&usqp=CAU',
+  },
+  {
+    id: 5,
+    name: 'Mobile',
+    imageUrl:
+      'https://5.imimg.com/data5/SELLER/Default/2021/1/HK/LE/LL/1073409/oppo-mobile-a9-2020-500x500.jpg',
+  },
+  {
+    id: 6,
+    name: 'Car',
+    imageUrl:
+      'https://pv-magazine-usa.com/wp-content/uploads/sites/2/2019/10/FordEV-1200x800.jpeg',
+  },
+];
+export const ALLPRODUCTS = [
+  {
+    id: 9,
+    name: 'Authority Large Breed Adult Dog Food - Lamb & Rice',
+    priceUnit: '77.00₪',
+    priceReduce: '',
+    productId: 12,
+    productCount: 1,
+    quantity: 1,
+    description:
+      'Provide your large breed dog with a delicious and nutritious meal time choice by serving Authority Everyday Health Lamb & Rice Formula Large Breed Adult Dog Food. This delicious dry blend supports joint health and mobility, a healthy immune system, healthy skin and a shiny coat, digestive health, and oral care.\n',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/9/image_1920?unique=20220304162131',
+  },
+  {
+    id: 7,
+    name: "Hill's® Science Diet® Dog Treat",
+    priceUnit: '25.00₪',
+    priceReduce: '',
+    productId: 10,
+    productCount: 1,
+    quantity: 1,
+    description:
+      "Spoil your canine companion with these Hill's Science Diet\nGrain-Free Dog Treats with Chicken & Apples, which are made\nwithout grains and are free of artificial colors and flavors, so\nyou can feel good about offering them to you dog. The all-natural\ningredients, including real chicken and apples, in these crunchy\ntreats will satisfy your pooch's cravings.\n\nFeatures: Made with real chicken and apples for a tasty\nsnack; grain-, corn- and artificial color- and flavor-free recipe;\npreserved naturally; crunchy\n\n",
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/7/image_1920?unique=20220304162215',
+  },
+  {
+    id: 5,
+    name: 'K&H Pet Products Self-Warming Pet Pad - Thermal Cat and Dog Warming Bed Mat',
+    priceUnit: '240.00₪',
+    priceReduce: '',
+    productId: 6,
+    productCount: 2,
+    quantity: 1,
+    description:
+      "SELF-WARMING TECHNOLOGY: Unique self-warming plush material captures your pet's body heat and radiates the warmth back to your dog or cat for a cozy self heating pet mat\nALL DAY COMFORT: Soft, plush reversible sides to match any décor and fit on furniture or inside your pets bed and or crate\nSLEEP SURFACE: Soft microfleece top creates a warm, cozy and comfortable cat sleeping bed\nNO ELECTRICITY NEEDED: Safely and naturally keeps your pet warm using their own comfortable and cozy body temperature\nEASY CARE: Machine washable cat bed for easy maintenance and convenient cat warming mat\nBRAND YOU TRUST: Designed by K&H with 20 years of experience in creating innovative, quality products",
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/5/image_1920?unique=20220304162244',
+  },
+  {
+    id: 11,
+    name: 'Orthopedic Cuddler Striped Dog Bed',
+    priceUnit: '180.00₪',
+    priceReduce: '',
+    productId: 14,
+    productCount: 1,
+    quantity: 1,
+    description:
+      'Your dog will find the optimal comfort he deserves on this Top Paw Tan Striped Orthopedic Cuddler Dog Bed. This great-looking dog bed features a removable pillow, and is soft and plush for true relaxation when your dog lays his head down to rest. Only at PetSmart.\n',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/11/image_1920?unique=20220304162258',
+  },
+  {
+    id: 2,
+    name: 'Pedigree Adult Dry Dog Food, Chicken Flavor, All Bag Sizes',
+    priceUnit: '70.00₪',
+    priceReduce: '',
+    productId: 2,
+    productCount: 2,
+    quantity: 1,
+    description:
+      'Contains one (1) 33 lb. bag of PEDIGREE Complete Nutrition Adult Dry Dog Food, Roasted Chicken, Rice and Vegetable Flavor\nThis dry food recipe helps maintain a healthy lifestyle with antioxidants, vitamins, and minerals, in the delicious chicken flavor dogs love\nProvides whole grains and helps support healthy digestion\nDelivers complete and balanced nutrition enriched with omega-6 fatty acids to help nourish your dog’s skin and coat\nMade in the USA with the World’s Finest Ingredients\nPEDIGREE Complete Nutrition Adult Dry Dog Food is made with no high fructose corn syrup, no artificial flavors, and no added sugar',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/2/image_1920?unique=20220304162318',
+  },
+  {
+    id: 10,
+    name: 'Perfect Weight Adult Wet Dog Food - Vegetable & Chicken Stew',
+    priceUnit: '1.00₪',
+    priceReduce: '',
+    productId: 13,
+    productCount: 1,
+    quantity: 1,
+    description:
+      "Helping your grown dog keep a healthy weight doesn't have to be difficult. Hill's Science Diet Adult Perfect Weight Hearty Vegetable & Chicken Stew is breakthrough weight management nutrition with clinically proven technology for healthy weight maintenance and long-lasting weight support in your adult dog. Over 70% of dogs lost weight within 10 weeks when fed this dog food made with natural ingredients. This premium canine recipe is also a great source of high-quality protein to support lean muscle in your dog. There's more to love with the new look of Science Diet.\n",
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/10/image_1920?unique=20220304162334',
+  },
+  {
+    id: 6,
+    name: 'Pet Products Bucket Booster Pet Seat - Elevated Pet Booster Seat',
+    priceUnit: '110.00₪',
+    priceReduce: '',
+    productId: 8,
+    productCount: 2,
+    quantity: 1,
+    description:
+      'DOG SEAT WITH A VIEW: Booster pet car seat elevates pet(s) for a better view, while the firm foam bolster around the edge keeps them in a safe and controlled spot for a distraction-free drive\nSAFETY FIRST: Compatible with any car seat belt to securely buckle in place with the seat belt hidden under the removable booster cushion plus comes with two adjustable security tethers to attached to dog harness\nQUICK INSTALLATION: Quick to install and uninstall and the front dips down so your pet can easily climb in and out before buckling up\nEASY CLEAN-UP: Removable, washable, plush quilted fleece cover ensures pet comfort and easy maintenance\nSIZING: The contoured back is designed to fit snugly in the front or back seat of any size car. The small bucket booster is recommended for small pets and the large booster can fit up to two small pets or one medium sized breed\nBRAND YOU TRUST: Designed by K&H with OVER 20 years of experience in creating innovative, quality products.',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/6/image_1920?unique=20220304162350',
+  },
+  {
+    id: 8,
+    name: 'Simply Nourish® Soft Chews Sticks Dog Treat - Natural',
+    priceUnit: '32.00₪',
+    priceReduce: '',
+    productId: 11,
+    productCount: 1,
+    quantity: 1,
+    description:
+      'Make treat time delicious and nutritious for your dog by serving Simply Nourish Chicken & Cheese Recipe Soft Chews Dog Treats. These delicious treats feature the great taste of chicken that dogs love in each and every bite, and are made using nutritious, wholesome ingredients you can feel good about serving to your dog.\n',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/8/image_1920?unique=20220304162405',
+  },
+  {
+    id: 3,
+    name: 'Test1',
+    priceUnit: '1.00₪',
+    priceReduce: '',
+    productId: 4,
+    productCount: 1,
+    quantity: 1,
+    description: '',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/3/image_1920?unique=20220224213931',
+  },
+  {
+    id: 4,
+    name: 'Test3',
+    priceUnit: '1.00₪',
+    priceReduce: '',
+    productId: 5,
+    quantity: 1,
+    productCount: 1,
+    description: '',
+    thumbNail:
+      'https://palco.tajr.io/web/image/product.template/4/image_1920?unique=20220224214005',
+  },
+];
